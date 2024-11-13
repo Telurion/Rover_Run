2a33f3dc88489b3be1d616d944dad21f7d1ea368
Telurion <telurion.ytb@gmail.com>
2024-11-13 13:41:09 +0100

    Node almost done

--- draw.h
+++ draw.h
@@ -12,15 +12,14 @@
 typedef struct s_node
 {
     int cost;
-    int index;                      //index
-    int nb_sons;                    //nb fils
-    int nb_remaining_moves;
+    int index;
+    int nb_sons;
 
     struct s_node **sons;           //pointeur tous ce qui suivent (sur le fils)
     struct s_node *parent;          //pointeur sur parent
     t_localisation loc;             //position : t_localisation
-    t_move precedent_move;          //mouvement précédent
-    t_move* remaining_moves;         //possibilités de mouvement
+    t_move precedent;          //mouvement précédent
+    t_move* remaining;         //possibilités de mouvement
 };
 typedef struct s_node t_node, *p_node;
 
