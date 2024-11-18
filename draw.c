fbfc061059aa9566d1644c90c4e146dc3d735e64
4m5n6 <amine.frikha@yahoo.com>
2024-11-19 00:59:13 +0100

    update du nouveau moves.c

--- draw.c
+++ draw.c
@@ -4,16 +4,14 @@
 
 #include "draw.h"
 #include "moves.h"
+#include "stack.h"
+#include "queue.h"
 #include "map.h"
 #include "loc.h"
 #include <stdlib.h>
 
 #define NUM_MOVEMENTS 7
 #define PHASES 9
-
-int isInMap(t_localisation loc, t_map map) {
-
-}
 
 t_move *getMovesArray() {
     int prob[NUM_MOVEMENTS] = {22, 37, 44, 51, 72, 93, 100};
@@ -22,9 +20,11 @@ t_move *getMovesArray() {
     if (!moves) {
         return NULL;
     }
+
     for (int i=0; i < PHASES; i++) {
         int val = rand() % prob[6] + 1;
         int temp=-1;
+
         if (val <= prob[0]) {
             moves[i] = F_10;
             temp = 0;
@@ -50,6 +50,7 @@ t_move *getMovesArray() {
             free(moves);
             return NULL;
         }
+
         if (temp > 0) {
             for (int j = temp; j < NUM_MOVEMENTS; j++) {
                 prob[j]--;
@@ -59,31 +60,24 @@ t_move *getMovesArray() {
     return moves;
 }
 
-t_move *reduceMovesArray(t_move *moves, t_move move, int length) {
-    t_move* new_moves = (t_move*) malloc(length*sizeof(t_move)-1);
-    int j = 0;
-    int found = 0;
-    for (int i = 0; i < length; i++) {
-        if (move != moves[i] && found == 0) {
-            new_moves[j] = moves[i];
-        }
-        else {
-            new_moves[j]= moves[i-1];
-        }
-    }
-    return new_moves;
-}
-
-p_node createNode(int idx, t_localisation loc, t_map map, t_node *parent, t_move precedent_move, t_move *remaining_move){
+p_node createNode(int idx, t_localisation loc, t_map map, t_node *parent){
     t_node *my_node;
     my_node = (t_node *)malloc(sizeof(t_node));
     my_node->cost = map.costs[loc.pos.x][loc.pos.y];
     my_node->index = idx;
     my_node->nb_sons = PHASES-idx;
+    my_node->nb_remaining_moves;
     my_node->sons = (t_node **)malloc((PHASES-idx)*sizeof(t_node *));
     my_node->parent = parent;
-    my_node->loc = loc;
-    my_node->precedent = precedent_move;
-    my_node->remaining = reduceMovesArray(remaining_move, my_node->precedent, my_node->nb_sons);
+
+    my_node->loc = loc;             //position : t_localisation
+    my_node->precedent_move;
+    my_node->remaining_moves;
     return my_node;
 }
+
+t_move *reduceMovesArray(t_move *moves, t_move move) {
+
+}
+
+//Faire fonction avec un tableau de moov et un moov donné et qui renvoie le tableau sans le moov donné
