ad645da67685b1a2f8ef9f3e99f2e24d636f83f8
Telurion <77536426+Telurion@users.noreply.github.com>
2024-11-21 21:58:57 +0100

    Working on tree

--- tree.h
+++ tree.h
@@ -1,5 +1,35 @@
 //
-// Created by amine on 19/11/2024.
+// Created by Corentin Monsallier on 21/11/2024.
 //
 
+#ifndef UNTITLED1_TREE_H
+#define UNTITLED1_TREE_H
 
+#include "stack.h"
+#include "map.h"
+#include "moves.h"
+
+typedef struct s_node
+{
+    int cost;
+    int nb_sons;
+    t_move move;
+    struct s_node **sons;           //pointeur tous ce qui suivent (sur le fils)
+    struct s_node *parent;          //pointeur sur parent
+} t_node;
+typedef struct s_node t_node, *p_node;
+
+typedef struct
+{
+    t_node *root;
+} t_tree;
+
+p_node createNode(int cost, int nb_sons, t_move move, t_node **sons, t_node *parent);
+t_tree createEmptyTree(int val_root);
+t_move *getMovesArray();
+//void addNodesToTree(p_node parent, t_move *remaining_moves);
+t_tree createTreeWithCombinations(t_move *, int);
+void printTree(p_node node, int depth);
+
+
+#endif //UNTITLED1_TREE_H
